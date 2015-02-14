5b35623a0c702d485caacab9a0dec0c7bbf4dfcd
Takashi Ogura <t.ogura@gmail.com>
2015-02-14 22:03:45 +0900

    Add pepper_utils.py

--- scripts/myo_arm.py
+++ scripts/myo_arm.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import rospy
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
 from std_msgs.msg import UInt8
+from otl_pepper_apps.pepper_utils import RosPepperProxy
+from otl_pepper_apps.pepper_utils import RosPepperUtil
+
 
 class MyoToArm:
     def __init__(self):
         self._imu_sub = rospy.Subscriber('/myo_imu', Imu, self.get_myo_rpy)
         self._gesture_sub = rospy.Subscriber('/myo_gest', UInt8,
                                              self.get_myo_gesture)
+        self._pepper_proxy = RosPepperProxy()
+        self._pepper_util = RosPepperUtil(self._pepper_proxy)
+        self._r_angle = -0.2
+        self._p_angle = 1.0
+        self._h_angle = 0.0
+        self._r_start_time = rospy.Time.now()
 
     def get_myo_gesture(self, gesture_msg):
         if gesture_msg.data == 3:
-            print 'nandeyanen!'
+            # if hand is open
+            if self._h_angle > 0.0:
+                self._pepper_proxy.speak('なんでやねん')
+                self._r_angle = -1.5
+                self._r_start_time = rospy.Time.now()
         elif gesture_msg.data == 4:
-            print 'pa-'
+            self._h_angle = 1.0
         elif gesture_msg.data == 1:
-            print 'gu-'
+            self._h_angle = 0.0
 
     def get_myo_rpy(self, imu_msg):
         y, p, r = euler_from_quaternion((imu_msg.orientation.w,
                                          imu_msg.orientation.x,
                                          imu_msg.orientation.y,
                                          imu_msg.orientation.z))
+        #self._r_angle = r
+        self._p_angle = p
         # r -> shoulder-r
         # p -> shoulder-p
         #print '%f, %f, %f'%(r, p, y)
 
+    def send_angles(self):
+        if ((rospy.Time.now() - self._r_start_time).to_sec() > 2):
+            self._r_angle = -0.2
+        self._pepper_proxy.move_joints(['RShoulderPitch', 'RShoulderRoll', 'RHand'],
+                                       [self._p_angle, self._r_angle, self._h_angle], speed=0.6)
+        print '%f, %f, %f' % (self._p_angle, self._r_angle, self._h_angle)
+
 
 rospy.init_node('myo_arm')
 m = MyoToArm()
-rospy.spin()
+r = rospy.Rate(10.0)
+while not rospy.is_shutdown():
+    r.sleep()
+    m.send_angles()
+
+    
---
+++ src/otl_pepper_apps/pepper_utils.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import rospy
+from naoqi_msgs.msg import JointAnglesWithSpeedActionGoal
+from naoqi_msgs.srv import GetInstalledBehaviors
+
+from std_msgs.msg import String
+from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Twist
+
+class RosPepperProxy(object):
+    JOINT_NAMES = ['HeadYaw', 'HeadPitch',
+                   'LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw', 'LHand',
+                   'RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'RHand',
+                   'HipRoll', 'HipPitch', 'KneePitch']
+    def __init__(self):
+        self._joint_publisher = rospy.Publisher('/joint_angles_action/goal',
+                                                JointAnglesWithSpeedActionGoal, queue_size=100)
+        self._speech_publisher = rospy.Publisher('/speech', String, queue_size=1)
+        self._base_move_pose_publisher = rospy.Publisher('/cmd_pose', Pose2D, queue_size=100)
+        self._base_move_twist_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=100)
+
+    def move_joints(self, names, angles, speed=0.3):
+        goal = JointAnglesWithSpeedActionGoal()
+        goal.goal.joint_angles.joint_names = names
+        goal.goal.joint_angles.joint_angles = angles
+        goal.goal.joint_angles.speed = speed
+        for name in names:
+            if name not in self.JOINT_NAMES:
+                rospy.logerr('%s is invalid joint name' % name)
+                return
+        self._joint_publisher.publish(goal)
+
+    def speak(self, string):
+        self._speech_publisher.publish(String(data=string))
+
+    def get_behaviors(self):
+        rospy.wait_for_service('/get_installed_behaviors')
+        get_behaviors = rospy.ServiceProxy('/get_installed_behaviors', GetInstalledBehaviors)
+        return get_behaviors().behaviors
+        
+    def move_to(self, x, y, theta):
+        self._base_move_pose_publisher.publish(Pose2D(x=x, y=y, theta=theta))
+
+    def move_vel(self, x, y, theta):
+        vel = Twist()
+        vel.linear.x = x
+        vel.linear.y = y
+        vel.angular.z = theta
+        self._base_move_twist_publisher.publish(vel)
+
+
+class RosPepperUtil(object):
+    def __init__(self, proxy):
+        self._proxy = proxy
+
+    def move_right_hand(self, angle, speed=0.3):
+        self._proxy.move_joints(['RHand'], [angle], speed=speed)
+
+    def move_left_hand(self, angle, speed=0.3):
+        self._proxy.move_joints(['LHand'], [angle], speed=speed)
+
+    def move_head(self, yaw, pitch, speed=0.15):
+        self._proxy.move_joints(['HeadYaw', 'HeadPitch'], [yaw, pitch], speed=speed)
+
+    def move_right_arm(self, shoulder_pitch, shoulder_roll, elbow_yaw,
+                       elbow_roll, wrist_yaw, speed=0.3):
+        self._proxy.move_joints(
+            ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw'],
+            [shoulder_pitch, shoulder_roll, elbow_yaw, elbow_roll, wrist_yaw],
+            speed=speed)
+
+    def move_left_arm(self, shoulder_pitch, shoulder_roll, elbow_yaw,
+                      elbow_roll, wrist_yaw, speed=0.3):
+        self._proxy.move_joints(
+            ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw'],
+            [shoulder_pitch, shoulder_roll, elbow_yaw, elbow_roll, wrist_yaw],
+            speed=speed)
+
